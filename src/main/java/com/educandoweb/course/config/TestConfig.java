2d82d526fa0d269e774c932271e7a1ce02f5b721
PedroAnjosPS <phpsanches@gmail.com>
2025-12-21 20:15:34 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -30,9 +31,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
-		Order o1 = new Order(null, Instant.parse("2025-12-21T15:00:00Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2025-11-05T06:30:45Z"), u1);
-		Order o3 = new Order(null, Instant.parse("2025-08-26T20:09:12Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2025-12-21T15:00:00Z"), 
+				OrderStatus.SHIPPED, u1);
+		Order o2 = new Order(null, Instant.parse("2025-11-05T06:30:45Z"), 
+				OrderStatus.DELIVERED, u1);
+		Order o3 = new Order(null, Instant.parse("2025-08-26T20:09:12Z"), 
+				OrderStatus.CANCELED, u2);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
