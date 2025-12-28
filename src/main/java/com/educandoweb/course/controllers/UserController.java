93aad25767b4870eead6aa6e532dcf806380c6e5
PedroAnjosPS <phpsanches@gmail.com>
2025-12-27 21:35:00 -0300

    Adding the update operation to the UserController

--- src/main/java/com/educandoweb/course/controllers/UserController.java
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +23,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	// INSERT
 	@GetMapping
 	public String listUsers(Model model) {
 		List<User> users = userRepository.findAll();
@@ -33,9 +35,7 @@ public class UserController {
 	
 	@GetMapping("/new") 
 	public String showCreateForm(Model model) {
-		List<User> users = userRepository.findAll();
-		model.addAttribute("users", users);
-		model.addAttribute("user", new User());
+		model.addAttribute("users", userRepository.findAll());
 		model.addAttribute("showForm", true);
 		return "users";
 	}
@@ -43,6 +43,20 @@ public class UserController {
 	@PostMapping("/create")
 	public String insert(User user) {
 		userService.insert(user);
+		return "redirect:/users";
+	}
+	
+	// UPDATE
+	@GetMapping("edit/{id}")
+	public String editUser(@PathVariable Long id, Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		model.addAttribute("editId", id);
+		return "users";
+	}
+	
+	@PostMapping("/update/{id}")
+	public String updateUser(@PathVariable Long id, User user) {
+		userService.update(id, user);
 		return "redirect:/users";
 	}
 }
